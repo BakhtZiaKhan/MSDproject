a9aa82a7228819e5b78469a2e7c61a56d5a086b8
Bakht <bakhtziakhan@gmail.com>
2023-12-12 02:57:20 +0000

    exercise images fixed

--- app/src/main/java/com/example/msd/ExerciseRVModal.java
+++ app/src/main/java/com/example/msd/ExerciseRVModal.java
@@ -10,11 +10,16 @@ public class ExerciseRVModal {
     private int calories, time; // Calories burned and time duration of the exercise
 
     // Constructor to create an instance of ExerciseRVModal.
+
     public ExerciseRVModal(String exerciseName, String exerciseDescription, int imgResId, int calories, int time) {
         this.exerciseName = exerciseName;
-
+        this.exerciseDescription = exerciseDescription;
+        this.imgResId = imgResId; // Changed from String to int
+        this.calories = calories;
         this.time = time;
     }
+
+
 // End of reference
     // Getter and setter methods for exercise name.
     public String getExerciseName() {
@@ -30,5 +35,14 @@ public class ExerciseRVModal {
     }
     public void setTime(int time) {
         this.time = time;
+    }
+
+
+    public int getImgResId() { // Changed return type to int
+        return imgResId;
+    }
+
+    public void setImgResId(int imgResId) { // Changed parameter type to int
+        this.imgResId = imgResId;
     }
 }
